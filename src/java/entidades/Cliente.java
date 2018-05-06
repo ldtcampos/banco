efe65138a1a55dcf5e19c06d7d8d518a7abae7d1
leandraT <leandraT@mail.com>
2018-05-06 14:48:05 -0600

    Proyecto Banco con clase Cliente

---
+++ src/java/entidades/Cliente.java
@@ -0,0 +1,40 @@
+
+package entidades;
+
+
+public class Cliente {
+    private int IdCliente;
+    private String Nombrecliente;
+    private String NIT;
+
+    public Cliente() {
+    }
+
+    public int getIdCliente() {
+        return IdCliente;
+    }
+
+    public void setIdCliente(int IdCliente) {
+        this.IdCliente = IdCliente;
+    }
+
+    public String getNombrecliente() {
+        return Nombrecliente;
+    }
+
+    public void setNombrecliente(String Nombrecliente) {
+        this.Nombrecliente = Nombrecliente;
+    }
+
+    public String getNIT() {
+        return NIT;
+    }
+
+    public void setNIT(String NIT) {
+        this.NIT = NIT;
+    }
+
+    
+    
+    
+}
